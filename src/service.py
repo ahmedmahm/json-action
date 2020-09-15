3d6c6e80b10e0c42a0438c24337bd8bd93622ff3
Ahmed-Mahmoud <ahmedm.monte@gmail.com>
2020-09-15 23:20:23 +0200

    changes

--- src/service.py
+++ src/service.py
@@ -5,11 +5,12 @@ print("::debug:: GETTING VARIABLES")
 workspace_name = os.environ.get("INPUT_WORKSPACE_NAME", default=None)
 resource_group = os.environ.get("INPUT_RESOURCE_GROUP", default=None)
 
-
 workspace_dict = {
     'name'           : workspace_name,
     'resource_group' : resource_group
 }
-with open("workspace.json", "w") as outfile: 
-    json.dump(workspace_dict, outfile) 
-print(f"::set-output name=workspace::{outfile}")
+
+f = json.dumps(workspace_dict)
+
+print('::set-output name=workspace::{f}')
+
